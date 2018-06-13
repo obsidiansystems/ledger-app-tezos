44c9450077534ee0bad4a9888f697cfd37213e20
Jimmy Hartzell <jimmy.hartzell@obsidian.systems>
2018-06-13 15:10:03 -0400

    display base58 hash of public key in ledger UI

--- src/display.c
+++ src/display.c
@@ -1,24 +1,63 @@
 #include "display.h"
 
-static inline char to_hexit(char in) {
-    if (in < 0xA) {
-        return in + '0';
-    } else {
-        return in - 0xA + 'A';
-    }
-}
+#include "base58.h"
+#include "blake2.h"
+
+#include "os.h"
+#include "cx.h"
 
 int convert_address(char *buff, uint32_t buff_size, void *raw_bytes, uint32_t size) {
-    if (size * 2 + 1 > buff_size) {
-        return 0;
+    const char type = *(char*)raw_bytes;
+
+    // Data to encode
+    const int HASH_SIZE = 20;
+    struct __attribute__((packed)) {
+        char prefix[3];
+        char hash[HASH_SIZE];
+        char checksum[4];
+    } data;
+
+    // prefix
+    data.prefix[0] = 6;
+    data.prefix[1] = 161;
+    switch (type) {
+        case 0x02: // Ed25519
+            data.prefix[2] = 159;
+            break;
+        case 0x04: // Secp256k1
+            data.prefix[2] = 161;
+            break;
     }
 
-    uint8_t *bytes = raw_bytes;
-    uint32_t i;
-    for (i = 0; i < size; i++) {
-        buff[i * 2] = to_hexit(bytes[i] >> 4);
-        buff[i * 2 + 1] = to_hexit(bytes[i] & 0xF);
+    // hash
+    switch (type) {
+        case 0x02: // Ed25519
+            {
+                // Already compressed
+                blake2b(data.hash, sizeof(data.hash), raw_bytes + 1, size - 1, NULL, 0);
+                break;
+            }
+        case 0x04: // Secp256k1
+            {
+                // Compress before hashing
+                char *bytes = raw_bytes;
+                char parity = bytes[64] & 0x01;
+                bytes[0] = 0x02 + parity;
+                size = 33;
+                blake2b(data.hash, sizeof(data.hash), bytes, size, NULL, 0);
+
+                // Restore original data
+                bytes[0] = 0x04;
+                break;
+            }
     }
-    buff[i * 2] = '\0';
-    return i * 2 + 1;
+
+    // checksum -- twice because them's the rules
+    uint8_t checksum[32];
+    cx_hash_sha256(&data, sizeof(data) - sizeof(data.checksum), checksum, sizeof(checksum));
+    cx_hash_sha256(checksum, sizeof(checksum), checksum, sizeof(checksum));
+    memcpy(data.checksum, checksum, sizeof(data.checksum));
+
+    b58enc(buff, &buff_size, &data, sizeof(data));
+    return buff_size;
 }
