1599106c75d7a3df4acd3811550c95ff5088158e
Vincent Bernardoff <vb@luminar.eu.org>
2018-06-27 15:47:06 +0200

    version: semantic versioning + app class

---
+++ src/version.h
@@ -0,0 +1,18 @@
+#ifdef BAKING_APP
+#define CLASS 1
+#else
+#define CLASS 0
+#endif
+
+#define MAJOR 0
+#define MINOR 1
+#define PATCH 0
+
+typedef struct version {
+    uint8_t class;
+    uint8_t major;
+    uint8_t minor;
+    uint8_t patch;
+} version_t;
+
+const version_t version = { CLASS, MAJOR, MINOR, PATCH };
