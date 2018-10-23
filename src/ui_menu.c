a6e4d0e499ff548f0ad07cdd2fefe7037e341b47
Jimmy Hartzell <jimmy.hartzell@obsidian.systems>
2018-10-23 15:25:21 -0400

    added icons

--- src/ui_menu.c
+++ src/ui_menu.c
@@ -1,5 +1,6 @@
 #include "ui_menu.h"
 
+#include "glyphs.h"
 #include "ui.h"
 
 #include "os.h"
@@ -17,14 +18,14 @@ static const ux_menu_entry_t about_menu_data[];
 
 static const ux_menu_entry_t about_menu_data[] = {
     {NULL, NULL, 0, NULL, "Tezos Wallet", "Version " VERSION, 0, 0},
-    {main_menu_data, NULL, 0, NULL, "Back", NULL, 0, 0}, // TODO: Put icon for "back" in
+    {main_menu_data, NULL, 1, &C_icon_back, "Back", NULL, 61, 40}, // TODO: Put icon for "back" in
     UX_MENU_END
 };
 
 static const ux_menu_entry_t main_menu_data[] = {
     {NULL, NULL, 0, NULL, "Use wallet to", "view accounts", 0, 0},
     {about_menu_data, NULL, 0, NULL, "About", NULL, 0, 0},
-    {NULL, exit_app_cb, 0, 0, "Quit app", NULL, 0, 0}, // TODO: Put icon for "dashboard" in
+    {NULL, exit_app_cb, 0, &C_icon_dashboard, "Quit app", NULL, 50, 29}, // TODO: Put icon for "dashboard" in
     UX_MENU_END
 };
 
