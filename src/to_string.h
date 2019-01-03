7f9e86ec67424bc410985c76700c6d7820402b13
Jimmy Hartzell <jimmy.hartzell@obsidian.systems>
2019-01-03 17:44:04 -0500

    clean up return types

--- src/to_string.h
+++ src/to_string.h
@@ -10,15 +10,15 @@
 #include "cx.h"
 #include "ui.h"
 
-int pubkey_to_pkh_string(char *buff, uint32_t buff_size, cx_curve_t curve,
+bool pubkey_to_pkh_string(char *buff, uint32_t buff_size, cx_curve_t curve,
                          const cx_ecfp_public_key_t *public_key);
-size_t protocol_hash_to_string(char *buff, const size_t buff_size, const uint8_t hash[PROTOCOL_HASH_SIZE]);
-int parsed_contract_to_string(char *buff, uint32_t buff_size, const struct parsed_contract *contract);
+bool protocol_hash_to_string(char *buff, const size_t buff_size, const uint8_t hash[PROTOCOL_HASH_SIZE]);
+bool parsed_contract_to_string(char *buff, uint32_t buff_size, const struct parsed_contract *contract);
 
 #define MAX_INT_DIGITS 20
 
 // These take their number parameter through a pointer and take a length
-size_t number_to_string_indirect(char *dest, uint32_t buff_size, const uint64_t *number);
-size_t microtez_to_string_indirect(char *dest, uint32_t buff_size, const uint64_t *number);
+bool number_to_string_indirect(char *dest, uint32_t buff_size, const uint64_t *number);
+bool microtez_to_string_indirect(char *dest, uint32_t buff_size, const uint64_t *number);
 
 bool copy_string(char *dest, uint32_t buff_size, const char *src);
