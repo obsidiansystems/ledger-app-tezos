43f924754328b71499eba22b72c3ab71b8a29e6c
Matthew Bauer <mjbauer95@gmail.com>
2019-10-10 14:43:49 -0400

    Add michelson.h header

---
+++ src/michelson.h
@@ -0,0 +1,30 @@
+// Subset of Michelson constants, used in parsing manager.tz
+// operations.
+
+#pragma once
+
+#include "memory.h"
+
+#define MICHELSON_ADDRESS          0x036e
+#define MICHELSON_CONS             0x031b
+#define MICHELSON_DROP             0x0320
+#define MICHELSON_IMPLICIT_ACCOUNT 0x031e
+#define MICHELSON_KEY_HASH         0x035d
+#define MICHELSON_MUTEZ            0x036a
+#define MICHELSON_NIL              0x053d
+#define MICHELSON_NONE             0x053e
+#define MICHELSON_OPERATION        0x036d
+#define MICHELSON_PUSH             0x0743
+#define MICHELSON_SET_DELEGATE     0x034e
+#define MICHELSON_SOME             0x0346
+#define MICHELSON_TRANSFER_TOKENS  0x034d
+#define MICHELSON_UNIT             0x034f
+
+#define MICHELSON_BYTE_SEQUENCE 0x0a
+#define MICHELSON_STRING        0x01
+#define MICHELSON_DATA          0x02
+
+#define MICHELSON_PARAMS_SOME   0xff
+#define MICHELSON_PARAMS_NONE   0x00
+
+#define MAX_MICHELSON_SEQUENCE_LENGTH 100
