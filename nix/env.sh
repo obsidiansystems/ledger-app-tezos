1b6c973dc43241e722c4f5bb439485cc966f3495
Jacquin Mininger <jacquin.mininger@obsidian.systems>
2019-11-01 15:56:31 -0400

    update all uses of nix/env.sh

--- nix/env.sh
+++ nix/env.sh
@@ -8,5 +8,7 @@ root="$(git rev-parse --show-toplevel)"
 if [ $# -eq 0 ]; then
   exec nix-shell "$root" -A "nano.${target}.wallet"
 else
-  exec nix-shell "$root" -A "nano.${target}.wallet" --run "$(echo "$@")"
+  echo "HERE"
+  echo "$@"
+  exec nix-shell "$root" -A "nano.${target}.wallet" $@
 fi
