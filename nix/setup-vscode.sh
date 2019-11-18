fa5d184f85921bbff153408ac038e1c2c7c75d2d
Matthew Bauer <mjbauer95@gmail.com>
2019-11-18 16:19:38 -0500

    Update nixpkgs to latest nixos-unstable, with ledgerblue changes
    Latest nixpkgs has ledgerblue 0.1.29, which has the --offline flag.
    
    Also store nixpkgs in Ob-style thunk instead of .nix

--- nix/setup-vscode.sh
+++ nix/setup-vscode.sh
@@ -7,6 +7,6 @@ cd "$root"
 cp -f "$(nix-build -A env.s.ide.config.vscode --no-out-link)" "vscode.code-workspace"
 chmod +w "vscode.code-workspace"
 
-bear="$(nix-build -E '(import nix/nixpkgs.nix {}).bear' --no-out-link)/bin/bear"
+bear="$(nix-build -E '(import ./nix/dep/nixpkgs {}).bear' --no-out-link)/bin/bear"
 # c.f. https://github.com/rizsotto/Bear/issues/182
 nix/env.sh s "export LANG=C.UTF-8; export LC_CTYPE=C.UTF-8; make clean && \"$bear\" make; make clean"
--- nix/watch.sh
+++ nix/watch.sh
@@ -13,7 +13,7 @@ root="$(git rev-parse --show-toplevel)"
 
 watchdirs=("$root/default.nix" "$root/nix" "$root/Makefile" "$root/src" "$root/icons")
 
-inotifywait="$(nix-build "$root/nix/nixpkgs.nix" -A inotify-tools --no-out-link)/bin/inotifywait"
+inotifywait="$(nix-build "$root/nix/dep/nixpkgs" -A inotify-tools --no-out-link)/bin/inotifywait"
 while true; do
   "$root/nix/env.sh" "$target" <<EOF
     $@
