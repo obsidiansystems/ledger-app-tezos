d11792c7d16fa94f65b91d236afd055ca422d1c5
Jacquin Mininger <jacquin.mininger@gmail.com>
2019-11-21 11:45:36 -0500

    fix test bc obsidian baker address is now a named delegate not custom one

--- test/apdu-tests/named-delegates.sh
+++ test/apdu-tests/named-delegates.sh
@@ -19,14 +19,16 @@ UNNAMED DELEGATES
 Please verify that the following fields appear on the ledger:
 
 CONFIRM DELEGATION
-  Delegate: tz1eY5Aqa1kXDFoiebL28emyXFoneAoVg1zh
+  Delegate: tz1cCTB1S2UYtGVJyuTQ9ktnnkemuqaSwvZn
   Delegate name: Custom Delegate: please verify the address
+  (Ignore all of the other stuff)
 
 afterwards you can accept this delegation.
 EOF
 {
     echo 8004000311048000002c800006c18000000080000000
-    echo 8004810053034376b9304606f1dc37a507b7d2e730e60a3040389f57d2ccc3cf2520607c52d66e00cf49f66b9ea137e11818f2a78b4b6fc9895b4e50e80904f44e00ff00cf49f66b9ea137e11818f2a78b4b6fc9895b4e50
+    echo 8004810058035379ba9122785f71ec283a3b6398c05edc8f8d77eef885d167d22c50e9f9c26c6e00cf49f66b9ea137e11818f2a78b4b6fc9895b4e50c0843d9e1480ea30e0d403ff00b5a3c247300abfea1242d10f347c321f796c1b88
+    # echo 8004810053034376b9304606f1dc37a507b7d2e730e60a3040389f57d2ccc3cf2520607c52d66e00cf49f66b9ea137e11818f2a78b4b6fc9895b4e50e80904f44e00ff00cf49f66b9ea137e11818f2a78b4b6fc9895b4e50
 } | ./apdu.sh > /dev/null
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 cat <<EOF
