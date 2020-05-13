9363e8efe6ab385defd274e089fd2cd2bd362f73
Jacquin Mininger <jacquin.mininger@gmail.com>
2020-05-13 14:52:45 -0400

    bump flextesa tests to carthage; deprecate Babylon tests

--- test/flextesa-dev.nix
+++ test/flextesa-dev.nix
@@ -5,5 +5,5 @@ with import ../nix/dep/tezos-baking-platform {};
 tezos.mkWithSrc {
   net = /master;
   src = ../nix/dep/flextesa-dev;
-  sha256 = "1kcsqp6wh1ipddychkdjsh3iw6xnkvymasbysxxhpr2yh5lis9jn";
+  sha256 = "175rjg3yi6p08j31190xamdk0qwn3jyramvpzj52l9v3adc0pkvh";
 }
