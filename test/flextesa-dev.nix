5a244fa0ec94d5f5aeed2ba05bfd0a24473d3510
Jacquin Mininger <jacquin.mininger@gmail.com>
2020-06-02 15:43:41 -0400

    bump flextesa suite

--- test/flextesa-dev.nix
+++ test/flextesa-dev.nix
@@ -5,5 +5,5 @@ with import ../nix/dep/tezos-baking-platform {};
 tezos.mkWithSrc {
   net = /master;
   src = ../nix/dep/flextesa-dev;
-  sha256 = "1xqvggjz1sp6w89km3yrl6h6q8aqdknr07dr4aba4d1pb4r6b1py";
+  sha256 = "1yy7h7nxkzl6bdjc37nwn6p7cx19qiy29cg6pcvirqfrnqkav6rb";
 }
