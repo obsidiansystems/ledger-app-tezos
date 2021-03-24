174a4ca866d67947d4203b1ab53fd91cefa4c3c7
pscott <scott.piriou@ledger.fr>
2021-03-24 10:42:41 +0100

    Add addiitonal swapping safety checks

--- src/swap/is_safe_to_swap.c
+++ src/swap/is_safe_to_swap.c
@@ -9,7 +9,27 @@ bool is_safe_to_swap() {
     char tmp_dest[57] = {0};
     parsed_contract_to_string(tmp_dest, sizeof(tmp_dest), &op->operation.destination);
 
-    if (op->total_fee != swap_values.fees) {
+    if (op->has_reveal == false) {
+        PRINTF("Should have reveal set to true\n");
+        return false;
+    }
+    else if (op->signing.originated != 0) {
+        PRINTF("Should not be originated\n");
+        return false;
+    }
+    else if (op->operation.tag != OPERATION_TAG_BABYLON_TRANSACTION) {
+        PRINTF("Should be of type babylon transaction\n");
+        return false;
+    }
+    else if (op->signing.signature_type != SIGNATURE_TYPE_ED25519)  {
+        PRINTF("Signature type is not ED25519\n");
+        return false;
+    }
+    else if (op->total_storage_limit != 257) {
+        PRINTF("Storage Limit incorrect\n");
+        return false;
+    }
+    else if (op->total_fee != swap_values.fees) {
         PRINTF("Fees differ\n");
         return false;
     }
