014a56b5848ae2b1e9a9b306607cd2ba7c11b55d
pscott <scott.piriou@ledger.fr>
2021-06-09 14:43:59 +0200

    Do not check for storageLimit when swapping

--- src/swap/is_safe_to_swap.c
+++ src/swap/is_safe_to_swap.c
@@ -13,22 +13,23 @@ bool is_safe_to_swap() {
         PRINTF("Should not be originated\n");
         return false;
     } else if (op->operation.tag != OPERATION_TAG_BABYLON_TRANSACTION) {
-        PRINTF("Should be of type babylon transaction\n");
+        PRINTF("Expected a babylon transaction, got %d\n", op->operation.tag);
         return false;
     } else if (op->signing.signature_type != SIGNATURE_TYPE_ED25519) {
-        PRINTF("Signature type is not ED25519\n");
-        return false;
-    } else if (op->total_storage_limit >= 257) {
-        PRINTF("Storage Limit incorrect\n");
+        PRINTF("Expected type ED25519 signature, got %d\n", op->signing.signature_type);
         return false;
     } else if (op->total_fee != swap_values.fees) {
-        PRINTF("Fees differ\n");
+        PRINTF("Fees differ: expected %d, got %d\n", op->total_fee, swap_values.fees);
         return false;
     } else if (op->operation.amount != swap_values.amount) {
-        PRINTF("Amounts differ\n");
+        PRINTF("Amounts differ: expected %d, got %d\n", op->operation.amount, swap_values.amount);
         return false;
     } else if (strncmp((const char *) &tmp_dest, swap_values.destination, sizeof(tmp_dest))) {
-        PRINTF("Addresses differ\n");
+        PRINTF("Addresses differ: expected %.*H, got %.*H\n",
+               sizeof(tmp_dest),
+               tmp_dest,
+               sizeof(tmp_dest),
+               swap_values.destination);
         return false;
     }
     return true;
